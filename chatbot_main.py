8a270a10cb9729928184544dbda0fec366ebcebd
Edward Moreira <edward.moreira@yahoo.com>
2019-04-14 16:52:53 -0300

    Adiciona comentáríos

--- chatbot_main.py
+++ chatbot_main.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
+# Permite escrever no padrão PTBR
 
 from chatterbot import ChatBot #chatbot
 from chatterbot.trainers import ListTrainer #trainador
-import os
+import os #Sistema operacional
 
 #Quando já estiver treinado, podemos passar mais um parâmetro sendo read_only=True
 #Assim o ChatBot já entende que não há necessidade de treinamento
 bot = ChatBot('Magal_ChatBot', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
-trainador = ListTrainer(bot)
-trainador.train('chatterbot.corpus.portuguese')
+#Criamos o Bot passando o nome e seu tipo de treinamento
+treinador = ListTrainer(bot) #Passamos o Bot para a lista a ser treinada
+treinador.train('chatterbot.corpus.portuguese') #Determina que o treinamento será em Português
 
-for arq in os.listdir('conversas'):
+for arq in os.listdir('conversas'): #Loop dentro da pasta conversas
     chats = open('conversas/' + arq, 'r').readlines()
-    trainador.train(chats)
+    # Abre todos os arquivos dentro da pasta conversas e le todas as linhas que há neles
+    treinador.train(chats) #Treina e carrega todas as convesas
 
-while True:
-    requisicao = input('Você: ')
+while True: #Loop até que seja verdadeira a pergunta e resposta
+    requisicao = input('Você: ') #Input do usuário
 
-    resposta = bot.get_response(requisicao)
-    if float(resposta.confidence) > 0.5:
-        print('Bot: ' + str(resposta))
+    resposta = bot.get_response(requisicao) #Output apartir do Input do usuário
+    if float(resposta.confidence) > 0.5: #Determina uma porcentagem, se o bot está apto ou não a responder a pergunta
+        print('Magal_ChatBot: ' + str(resposta)) #Resposta pois houve uma taxa maior de aptidão proposta
     else:    
-        print('Bot: Desculpa ainda não consigo responder essa pergunta')
+        print('Magal_ChatBot: Desculpa ainda não consigo responder essa pergunta') #Informa que ainda não há dados suficiente para responder
     
